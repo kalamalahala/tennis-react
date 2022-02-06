9b2fee6e72187bef936f6675e48c8612e2ac8234
kalamalahala <solo.driver.bob@gmail.com>
2022-02-06 10:01:01 -0500

    Initialize React modules, create base component

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// initialize react required files
+import React from "react";
+import ReactDom from "react-dom";
+
+// Create first component function / class or whatever
+
+function Website() {
+  return (
+    <div className="container">
+      <h4>"Big Ol String"</h4>
+      <div className="francis">other content</div>
+    </div>
+  );
+}
+
+ReactDom.render(<Website />, document.getElementById("root"));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
