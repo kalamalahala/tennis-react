1533262c11fa7cafbc0bb2670f0e1092e0fda385
kalamalahala <solo.driver.bob@gmail.com>
2022-02-11 16:04:28 -0500

    added score ticker

--- src/comp/Marquee.component.js
+++ src/comp/Marquee.component.js
@@ -1,33 +1,47 @@
-import React from 'react';
-import Marquee from 'react-fast-marquee';
-import {scores} from '../json/matchScores'
+import React from "react";
+import Marquee from "react-fast-marquee";
+import MatchCard from "./MatchCard.component";
+import { scores } from "../json/matchScores";
 
 const scoreSelection = () => {
   let pickedScores = [];
   while (pickedScores.length < 10) {
-    let randomGame = Math.floor(Math.random()*scores.length);
-    pickedScores.push(scores[randomGame]);    
+    let randomGame = Math.floor(Math.random() * scores.length);
+    pickedScores.push(scores[randomGame]);
   }
   return pickedScores;
 };
 
 const scoreArray = scoreSelection();
 
-
-const TickerItem = ({scores}) => {
-          return <>
-            {scoreArray.map(score => (<div style={{margin:0, padding:"5px 15px"}} className="score" key={score.match_id}>
-              <span className='scoreName'>{score.winner_name} d. {score.loser_name}:</span> {score.match_score_tiebreaks}
-            </div>))}
-          </>
+const TickerItem = ({ scores }) => {
+  return (
+    <>
+      {scoreArray.map((score) => (
+        <div
+          style={{ margin: 0, padding: "5px 15px" }}
+          className="score"
+          key={score.match_id}
+        >
+          <span className="scoreName">
+            {score.winner_name} d. {score.loser_name}:
+          </span>{" "}
+          {score.match_score_tiebreaks}
+        </div>
+      ))}
+    </>
+  );
 };
 
-
-
 function UnderNavMarquee(props) {
-  return    <>
-                <Marquee><TickerItem /></Marquee>
-            </>;
+  return (
+    <>
+      <Marquee>
+        <TickerItem />
+      </Marquee>
+      <MatchCard></MatchCard>
+    </>
+  );
 }
 
 export default UnderNavMarquee;
---
+++ src/comp/MatchCard.component.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import { scores } from "../json/matchScores";
+
+const MatchCard = (props) => {
+  const { loser_name, match_score_tiebreaks, winner_name } = props;
+  return (
+    <>
+      <div className="matchCardContainer">
+        <div className="namesContainer">
+            <div className="winnerName">{winner_name}</div>
+            <div className="loserName">{loser_name}</div>            
+        </div>
+        <div className="scoreContainer">{match_score_tiebreaks}</div>
+      </div>
+    </>
+  );
+};
+
+export default MatchCard;
